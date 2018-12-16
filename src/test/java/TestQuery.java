9e56cb904b2ac7279b3606f037da37227aad79a6
Marcelo <marcelo.pinto14@hotmail.com>
2018-12-16 22:19:27 +0000

    ChangeDirectory Test

--- src/test/java/TestQuery.java
+++ src/test/java/TestQuery.java
@@ -1,4 +1,40 @@
 package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestQuery {
+
+    //Path null
+    @Test
+    public void TC1_1(){
+        String path = "null";
+        boolean resultado = false;
+        boolean valor;
+        Query query = new Query();
+        valor = query.changeDirectory(path);
+        assertEquals(resultado,valor);
+    }
+
+    //Pasta vazia
+    @Test
+    public void TC1_2() {
+        String path = "C:\\Users\\windows\\Desktop\\Nova Pasta";
+        boolean resultado = false;
+        boolean valor;
+        Query query = new Query();
+        valor = query.changeDirectory(path);
+        assertEquals(resultado, valor);
+    }
+
+    //Pasta com ficheiro
+    @Test
+    public void TC1_3(){
+        String path = "C:\\Users\\windows\\Desktop\\ola";
+        boolean resultado = true;
+        boolean valor;
+        Query query = new Query();
+        valor = query.changeDirectory(path);
+        assertEquals(resultado,valor);
+    }
+
 }
